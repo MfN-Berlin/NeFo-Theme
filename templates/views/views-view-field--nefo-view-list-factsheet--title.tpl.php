16a6143776d5c58edbec5871df1adf5e717470f7
AntonioPonte <antonio.ponte@MFN0134.naturkundemuseum-berlin.de>
2016-02-17 10:31:04 +0100

    Add view templates for fields.

---
+++ templates/views/views-view-field--nefo-view-list-factsheet--title.tpl.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * This template is used to print a single field in a view.
+ *
+ * It is not actually used in default Views, as this is registered as a theme
+ * function which has better performance. For single overrides, the template is
+ * perfectly okay.
+ *
+ * Variables available:
+ * - $view: The view object
+ * - $field: The field handler object that can process the input
+ * - $row: The raw SQL result that can be used
+ * - $output: The processed output that will normally be used.
+ *
+ * When fetching output from the $row, this construct should be used:
+ * $data = $row->{$field->field_alias}
+ *
+ * The above will guarantee that you'll always get the correct data,
+ * regardless of any changes in the aliasing that might happen if
+ * the view is modified.
+ */
+
+if (!empty($row)) {
+	$path = drupal_get_path('theme', 'ofen');
+	$link = count($row->field_field_nefo_factsheet_link);
+	$factsheet_type = ($link) ? '' : '<img class="nefo-pdf-icon" alt="" title="application/pdf" src="/'. $path .'/images/icons/application-pdf.png">';
+	$output = $output .' '. $factsheet_type;
+}
+
+print $output;
+
+?>
+
+
