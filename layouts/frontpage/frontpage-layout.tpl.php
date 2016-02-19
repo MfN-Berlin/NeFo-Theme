40dde4b125c9ed1990ae2b25309caae9fcd76438
Sascha Gennrich <sascha.gennrich@mfn-berlin.de>
2016-02-19 13:48:42 +0100

    fix carousel positioning

--- layouts/frontpage/frontpage-layout.tpl.php
+++ layouts/frontpage/frontpage-layout.tpl.php
@@ -98,22 +98,16 @@
     </header>
 
     <div class="l-main">
+        <?php print render($page['highlighted']); ?>
+        <?php print render($page['featured']); ?>
+        <?php print render($page['slider']); ?>
+        <?php print $messages; ?>
         <div class="l-content" role="main">
-            <?php print render($page['highlighted']); ?>
             <a id="main-content"></a>
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?>
-                <h1><?php print $title; ?></h1>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php print $messages; ?>
-            <?php print render($tabs); ?>
             <?php print render($page['help']); ?>
             <?php if ($action_links): ?>
                 <ul class="action-links"><?php print render($action_links); ?></ul>
             <?php endif; ?>
-            <?php print render($page['featured']); ?>
-            <?php print render($page['slider']); ?>
             <?php print render($page['atlas']); ?>
             <?php print render($page['news']); ?>
             <?php print render($page['products']); ?>
