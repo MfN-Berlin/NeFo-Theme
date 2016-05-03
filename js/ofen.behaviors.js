30426928148348c1e964fd3dc378578e6e5342e5
Sascha Gennrich <sascha.gennrich@mfn-berlin.de>
2016-05-03 11:44:36 +0200

    remove em to px conversion

--- js/ofen.behaviors.js
+++ js/ofen.behaviors.js
@@ -58,16 +58,7 @@
   };
   Drupal.behaviors.moveTopMenu = {
     attach: function (context, settings) {
-      var getEmInPx = function (parentElement) {
-        parentElement = parentElement || document.body;
-        var div = document.createElement('div');
-        div.style.width = "1000em";
-        parentElement.appendChild(div);
-        var pixels = div.offsetWidth / 1000;
-        parentElement.removeChild(div);
-        return pixels;
-      }
-      var tabWidth = getEmInPx(document.body) * 44; //px size of 44em
+      var tabWidth = 540;
       var moveToFooter = function(event){
         var win = $(this);
         if (win.width() <= tabWidth) {
