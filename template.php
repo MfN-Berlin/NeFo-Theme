91355c73b2c3b7e44569d774d2e9a425b565f1b6
AntonioPonte <Antonio.Ponte@mfn-berlin.de>
2017-11-21 15:59:39 +0100

    #414: Standardanzeige der NeFo-Dokumente anpassen

--- template.php
+++ template.php
@@ -210,9 +210,57 @@ function ofen_item_list($variables) {
     return $output;
 }
 
+
 function ofen_preprocess_search_result(&$vars) {
     $date = $vars['result']['date'];
     $vars['info_split']['date'] = format_date($date, 'custom', 'j. F Y');
 }
+
+
+/**
+ * Open PDF file in new window
+ * See: https://www.drupal.org/node/301234
+ */
+function ofen_file_link($variables) {
+  $file = $variables['file'];
+  $icon_directory = drupal_get_path('theme', 'ofen') . '/images/icons';
+  $url = file_create_url($file->uri);
+  $file_size = format_size($file->filesize);
+  $icon = theme('file_icon', array('file' => $file, 'icon_directory' => $icon_directory));
+
+  // Set options as per anchor format described at
+  // http://microformats.org/wiki/file-format-examples
+  $options = array(
+    'attributes' => array(
+      'type' => $file->filemime . '; length=' . $file->filesize,
+    ),
+  );
+
+  // Use the description as the link text if available.
+  if (empty($file->description)) {
+    $link_text = $file->filename;
+  }
+  else {
+    $link_text = $file->description;
+    $options['attributes']['title'] = check_plain($file->filename);
+  }
+
+  // Open files of particular mime types in new window
+  $new_window_mimetypes = array('application/pdf','text/plain');
+  if (in_array($file->filemime, $new_window_mimetypes)) {
+    $options['attributes']['target'] = '_blank';
+  }
+
+  // File size
+  $link_text .= '<br />'. $file_size;
+  $options['html'] = TRUE;
+
+  return '<div class="file">'.
+				'<div class="file-icon">' . $icon . '</div>'.
+				'<div class="file-link">' . l($link_text, $url, $options) .'</div>'.
+        '</div>';
+}
+
+
 
 
