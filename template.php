4ac7f38edb4af40178e451a4c6c448b389582bdc
AntonioPonte <Antonio.Ponte@mfn-berlin.de>
2017-12-04 14:37:33 +0100

    remove unused superfish customization

--- template.php
+++ template.php
@@ -14,21 +14,8 @@ function startsWith($haystack, $needle) {
  * hook_form_FORM_ID_alter
  */
 function ofen_form_search_block_form_alter(&$form, &$form_state, $form_id) {
-//    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
-//    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
-//    $form['search_block_form']['#size'] = 40;  // define size of the textfield
-//    $form['search_block_form']['#default_value'] = t('Search'); // Set a default value for the textfield
-//    $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
-//    $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button-16x16.png');
-//
-//    // Add extra attributes to the text box
-//    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
-//    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
-//    // Prevent user from searching the default text
-//    $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
-//
-//    // Alternative (HTML5) placeholder attribute instead of using the javascript
-    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
+  // Alternative (HTML5) placeholder attribute instead of using the javascript
+  $form['search_block_form']['#attributes']['placeholder'] = t('Search');
 }
 
 /**
@@ -53,96 +40,6 @@ function ofen_form($variables) {
     // CHANGED: Anonymous DIV to satisfy XHTML compliance.
     //    return '<form' . drupal_attributes($element['#attributes']) . '><div>' . $element['#children'] . '</div></form>';
     return '<form' . drupal_attributes($element['#attributes']) . '>' . $element['#children'] . '</form>';
-}
-
-//function ofen_views_exposed_form($form) {
-//    $form['submit']['#value'] = t('Search');
-//    $output = drupal_render($form);
-//    return $output;
-//}
-
-/**
- * Theme function to allow any menu tree to be themed as a Superfish menu.
- */
-function ofen_superfish($variables) {
-    global $user, $language;
-
-    $id = $variables['id'];
-    $menu_name = $variables['menu_name'];
-    $mlid = $variables['mlid'];
-    $sfsettings = $variables['sfsettings'];
-
-    $menu = menu_tree_all_data($menu_name);
-
-    if (function_exists('i18n_menu_localize_tree')) {
-        $menu = i18n_menu_localize_tree($menu);
-    }
-
-    // For custom $menus and menus built all the way from the top-level we
-    // don't need to "create" the specific sub-menu and we need to get the title
-    // from the $menu_name since there is no "parent item" array.
-    // Create the specific menu if we have a mlid.
-    if (!empty($mlid)) {
-        // Load the parent menu item.
-        $item = menu_link_load($mlid);
-        $title = check_plain($item['title']);
-        $parent_depth = $item['depth'];
-        // Narrow down the full menu to the specific sub-tree we need.
-        for ($p = 1; $p < 10; $p++) {
-            if ($sub_mlid = $item["p$p"]) {
-                $subitem = menu_link_load($sub_mlid);
-                $key = (50000 + $subitem['weight']) . ' ' . $subitem['title'] . ' ' . $subitem['mlid'];
-                $menu = (isset($menu[$key]['below'])) ? $menu[$key]['below'] : $menu;
-            }
-        }
-    }
-    else {
-        $result = db_query("SELECT title FROM {menu_custom} WHERE menu_name = :a", array(':a' => $menu_name))->fetchField();
-        $title = check_plain($result);
-    }
-
-    $output['content'] = '';
-    $output['subject'] = '<i class="fa fa-bars"></i> ' . $title;
-    if ($menu) {
-        // Set the total menu depth counting from this parent if we need it.
-        $depth = $sfsettings['depth'];
-        $depth = ($sfsettings['depth'] > 0 && isset($parent_depth)) ? $parent_depth + $depth : $depth;
-
-        $var = array(
-          'id' => $id,
-          'menu' => $menu,
-          'depth' => $depth,
-          'trail' => superfish_build_page_trail(menu_tree_page_data($menu_name)),
-          'clone_parent' => FALSE,
-          'sfsettings' => $sfsettings,
-        );
-        if ($menu_tree = theme('superfish_build', $var)) {
-            if ($menu_tree['content']) {
-                // Add custom HTML codes around the main menu.
-                if ($sfsettings['wrapmul'] && strpos($sfsettings['wrapmul'], ',') !== FALSE) {
-                    $wmul = explode(',', $sfsettings['wrapmul']);
-                    // In case you just wanted to add something after the element.
-                    if (drupal_substr($sfsettings['wrapmul'], 0) == ',') {
-                        array_unshift($wmul, '');
-                    }
-                }
-                else {
-                    $wmul = array();
-                }
-                $output['content'] = isset($wmul[0]) ? $wmul[0] : '';
-                $output['content'] .= '<ul id="superfish-' . $id . '"';
-                $output['content'] .= ' class="menu sf-menu sf-' . $menu_name . ' sf-' . $sfsettings['type'] . ' sf-style-' . $sfsettings['style'];
-                $output['content'] .= ($sfsettings['itemcounter']) ? ' sf-total-items-' . $menu_tree['total_children'] : '';
-                $output['content'] .= ($sfsettings['itemcounter']) ? ' sf-parent-items-' . $menu_tree['parent_children'] : '';
-                $output['content'] .= ($sfsettings['itemcounter']) ? ' sf-single-items-' . $menu_tree['single_children'] : '';
-                $output['content'] .= ($sfsettings['ulclass']) ? ' ' . $sfsettings['ulclass'] : '';
-                $output['content'] .= ($language->direction == 1) ? ' rtl' : '';
-                $output['content'] .= '">' . $menu_tree['content'] . '</ul>';
-                $output['content'] .= isset($wmul[1]) ? $wmul[1] : '';
-            }
-        }
-    }
-    return $output;
 }
 
 /**
@@ -260,7 +157,4 @@ function ofen_file_link($variables) {
 				'<div class="file-link">' . l($link_text, $url, $options) .'</div>'.
         '</div>';
 }
-
-
-
 
